8c8d40f5bb6ce0e24adb6de1a731e700fc959cf9
alre87 <alberto.resca@outlook.it>
2025-01-20 19:01:02 +0100

    Create repository interface

---
+++ CadSimulation.Repository/FileSystemShapeRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CadSimulation.Core;
+
+namespace CadSimulation.Repository
+{
+    internal class FileSystemShapeRepository : IShapeRepository
+    {
+        public IEnumerable<IShape> LoadShapes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveShapes(IEnumerable<IShape> shapes)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CadSimulation.Repository/IShapeRepository.cs
@@ -0,0 +1,10 @@
+﻿using CadSimulation.Core;
+
+namespace CadSimulation.Repository
+{
+    public interface IShapeRepository
+    {
+        void SaveShapes(IEnumerable<IShape> shapes);
+        IEnumerable<IShape> LoadShapes();
+    }
+}
