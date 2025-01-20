bbd240419176cb9bc984d8209dd112eaa75f9224
alre87 <alberto.resca@outlook.it>
2025-01-20 19:14:32 +0100

    Implement service

---
+++ CadSimulation.Application/ShapeService.cs
@@ -0,0 +1,25 @@
+﻿using CadSimulation.Core;
+using CadSimulation.Repository;
+
+namespace CadSimulation.Application
+{
+    public class ShapeService
+    {
+        private readonly IShapeRepository _ShapeRepository;
+
+        public ShapeService(IShapeRepository shapeRepository)
+        {
+            _ShapeRepository = shapeRepository;
+        }
+
+        public void SaveShapes(IEnumerable<IShape> shapes)
+        {
+            _ShapeRepository.SaveShapes(shapes);
+        }
+
+        public IEnumerable<IShape> LoadShapes()
+        {
+            return _ShapeRepository.LoadShapes();
+        }
+    }
+}
